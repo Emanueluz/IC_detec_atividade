0a063c7c08efa322718687ea47409e8ecec66e7e
Emanueluz <luzemanuel.n@gmail.com>
2023-09-18 14:56:48 -0300

    asd

--- paho-mqtt-client-example-master/separate_publisher.py
+++ paho-mqtt-client-example-master/separate_publisher.py
@@ -19,13 +19,12 @@ ssl._create_default_https_context = ssl._create_unverified_context
 from paho import mqtt
 import paho.mqtt.client as paho
 import paho.mqtt.publish as publish
+from paho.mqtt import client as mqtt_client
+import paho.mqtt.client as mqtt
 # create a set of 2 test messages that will be published at the same time
 msgs = [{'topic': "paho/test/multiple", 'payload': "test 1"}, ("paho/test/multiple", "test 2", 0, False)]
 
-# use TLS for secure connection with HiveMQ Cloud
-sslSettings = ssl.SSLContext(mqtt.client.ssl.PROTOCOL_TLS_CLIENT)
-
+ 
 # put in your cluster credentials and hostname
 auth = {'username': "aaaaa", 'password': "Aa123456"}
-publish.multiple(msgs, hostname="b5b85536bc1e42009bf45c3e2997d02d.s2.eu.hivemq.cloud", port=8883, auth=auth,
-                 tls=sslSettings, protocol=paho.MQTTv31)
+client.publish(msgs, hostname="b5b85536bc1e42009bf45c3e2997d02d.s2.eu.hivemq.cloud", port=8883, auth=auth)
